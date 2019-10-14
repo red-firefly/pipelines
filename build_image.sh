46240e53b6bc09f3f345eb3922d406d69846e02f
Changjian Gao <gaochangjian@gmail.com>
2019-10-14 13:35:44 +0800

    Support custom TensorBoard image of viewer CRD by environment variable (#1)

---
+++ build_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+COMPONENT=$(python -c "print('${TRAVIS_TAG}'.split('-v')[0])")
+echo "Component is ${COMPONENT}"
+case "${COMPONENT}" in
+  api-server)
+    DOCKERFILE_PATH=backend/Dockerfile
+    ;;
+
+  viewer-crd-controller)
+    DOCKERFILE_PATH=backend/Dockerfile.viewercontroller
+    ;;
+
+  *)
+    echo "Unknown component ${COMPONENT}"
+    exit 1
+    ;;
+esac
+
+# BRANCH=$(git rev-parse --abbrev-ref HEAD | tr / - | tr _ -)
+# SHORT_COMMIT=$(git log -n 1 --pretty=format:'%h')
+IMAGE_TAG=$(python -c "print('v' + '${TRAVIS_TAG}'.split('-v')[1])")
+IMAGE_NAME=${DOCKER_IMAGE_ORG}/kubeflow-pipelines-${COMPONENT}:${IMAGE_TAG}
+echo "Image name is ${IMAGE_NAME}"
+echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin && \
+  docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH . && \
+  docker push $IMAGE_NAME
